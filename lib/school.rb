0fc1f2e19b2d07f10d3396b78e1c603147bc2859
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 17:08:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,5 +2,10 @@
 class School
   def initialize(name)
     @name = name
+    @roster = { :grade, :students }
+  end
+
+  def add_student(:grade, :student)
+    self.roster[:grade] << :student
   end
 end
